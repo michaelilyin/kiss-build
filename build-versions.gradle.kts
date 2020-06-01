e2b0247ff87e75d78ef1194c7a1d0d3fe4557cc5
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-06-01 15:09:51 +0300

    Migrated to webflux

--- build-versions.gradle.kts
+++ build-versions.gradle.kts
@@ -3,7 +3,7 @@
 allprojects {
     val kotlinVer by extra("1.3.70")
     val springbootDepPluginVer by extra("1.0.9.RELEASE")
-    val springbootVer by extra("2.2.5.RELEASE")
+    val springbootVer by extra("2.3.0.RELEASE")
     val buildinfoPluginVer by extra("0.1.3")
     val detektVer by extra("1.0.1")
     val ktlintVer by extra("9.1.0")
